aad8c32b537c9b1b1b8f8e98936f67bb37113d1e
Surya Tripathi <raj.surya19@gmail.com>
2012-07-31 17:43:23 +0530

    sc promotion final

--- app/controllers/checkout_controller_decorator.rb
+++ app/controllers/checkout_controller_decorator.rb
@@ -7,7 +7,7 @@ CheckoutController.class_eval do
   		products = @order.line_items.map { |li| li.variant.product }
   		promotion_ids = products.map(&:promotion_id).reject{ |promo_id| promo_id.blank? }
   		adjustment = @order.adjustments.where(:source_type => "Promotion").first
-  		if adjustment.nil?
+  		if adjustment.nil? && !promotion_ids.blank?
   			promotion_ids.each{ |promo_id| (@order.adjustments << Adjustment.create(:source_type => "Promotion", :source_id => promo_id, :amount => products.find{ |product| product.promotion_id == promo_id }.promotioned_amount, :label => "Coupon (#{products.find{ |product| product.promotion_id == promo_id }.promotion_code})")) }
   		else
 
