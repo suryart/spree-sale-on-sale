530c0ef0d8b1a5f6ea0c03c7ed1bf5eefcde68a5
Surya Tripathi <raj.surya19@gmail.com>
2012-07-31 13:19:07 +0530

    checkout controling and sos handler

--- app/models/sale_on_sale_import_handler.rb
+++ app/models/sale_on_sale_import_handler.rb
@@ -21,23 +21,17 @@ class SaleOnSaleImportHandler
 			end_date = (end_date.split(" ").first.split("/").last.size == 2) ?  DateTime.strptime("#{end_date}","%d/%m/%y %H:%M:%S") : DateTime.strptime("#{end_date}","%d/%m/%Y %H:%M:%S")
 			product = Variant.where(:sku => sku).first.try(:product)
 			unless product.nil?
-				puts "="*100
 				promotion_rule = product.promotion_rules.first
 				product_group = ProductGroup.find_or_create_by_name("SOS")
 				product.product_groups << product_group if product.product_groups.blank?
 				if promotion_rule.nil?
 					promotion = Promotion.new
 					promotion.update_attributes(:code => promotion.gen_code, :description => "Promotion Code for Sale on Sale", :usage_limit => product.variants.map(&:count_on_hand).sum, :combine => false, :match_policy => "any", :name => product.class.to_s, :starts_at => start_date, :expires_at => end_date)
-					puts promotion.inspect
-					puts "\n \n"
 					product.update_attributes(:promoted_amount => promoted_amount, :promotion_id => promotion.id)
-					puts product.inspect
-					puts "\n \n"
 					product.promotion_rules << Promotion::Rules::Product.create(:promotion_id => promotion.id, :user_id => @sale_on_sale_import.user_id, :product_group_id => product_group.id, :type => "Promotion::Rules::Product")
 					calculator = promotion.calculator.nil? ? Calculator::PerItem.create(:type => "Calculator::PerItem", :calculable_id => promotion.id, :calculable_type => promotion.class.to_s) : promotion.calculator
 					preference = Preference.find_or_create_by_owner_id_and_owner_type_and_name(calculator.id, "Calculator", "amount")
 					preference.update_attributes(:value => (product.price-product.promoted_amount))
-					puts "-"*100
 				else
 					calculator = promotion_rule.promotion.calculator
 					preference = Preference.where(:owner_id => calculator.id, :owner_type => "Calculator", :name => "amount").first
